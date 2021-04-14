46aa54641e063661bc9a45d99c5aa321d702cac2
景琦 <763297515@qq.com>
2021-04-14 14:43:37 +0800

    样式修改

--- src/app/pages/index/start-reading/start-reading.component.ts
+++ src/app/pages/index/start-reading/start-reading.component.ts
@@ -24,13 +24,9 @@ export class StartReadingComponent implements OnInit {
     this.expand = true;
     anime({
       targets: this.container.nativeElement,
-      borderRadius: {
-        value: ['50%', 0],
-        duration: 200
-      },
       backgroundColor: {
-        value: '#ffd1c2',
-        duration: 200
+        value: '#ffffff',
+        duration: 2000
       },
       width: '100vw',
       height: '100vh'
